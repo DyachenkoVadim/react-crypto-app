291c9de75efc10d53ed0a37a0fd42aad58b56f34
Vadim <vadimdyachenko000@gmail.com>
2024-02-21 18:41:36 +0200

    innit app

---
+++ frontend/src/App.jsx
@@ -0,0 +1,8 @@
+import {CryptoContextProvider} from './context/crypto-context.jsx';
+import AppLayout from './components/layout/AppLayout.jsx';
+
+export default function App() {
+  return  (<CryptoContextProvider>
+   <AppLayout></AppLayout>
+  </CryptoContextProvider>)
+}
